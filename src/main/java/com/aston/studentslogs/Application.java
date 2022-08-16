35a7a9c4c3eb9ef3acc7873effc4192ddb838725
moryi <mory.ivan@gmail.com>
2022-08-17 00:17:42 +0300

    Remove some method

--- src/main/java/com/aston/studentslogs/Application.java
+++ src/main/java/com/aston/studentslogs/Application.java
@@ -27,26 +27,4 @@ public class Application extends AbstractAnnotationConfigDispatcherServletInitia
             return new String[]{"/"};
         }
 
-        @Override
-        public void onStartup(ServletContext aServletContext) throws ServletException {
-            super.onStartup(aServletContext);
-            registerCharacterEncodingFilter(aServletContext);
-            registerHiddenFieldFilter(aServletContext);
-        }
-
-        private void registerHiddenFieldFilter(ServletContext aContext) {
-            aContext.addFilter("hiddenHttpMethodFilter",
-                    new HiddenHttpMethodFilter()).addMappingForUrlPatterns(null, true, "/*");
-        }
-
-        private void registerCharacterEncodingFilter(ServletContext aContext) {
-            EnumSet<DispatcherType> dispatcherTypes = EnumSet.of(DispatcherType.REQUEST, DispatcherType.FORWARD);
-
-            CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
-            characterEncodingFilter.setEncoding("UTF-8");
-            characterEncodingFilter.setForceEncoding(true);
-
-            FilterRegistration.Dynamic characterEncoding = aContext.addFilter("characterEncoding", characterEncodingFilter);
-            characterEncoding.addMappingForUrlPatterns(dispatcherTypes, true, "/*");
-        }
 }
