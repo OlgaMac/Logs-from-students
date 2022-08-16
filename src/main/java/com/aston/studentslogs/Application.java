332a09fa6b5bfd18fd5e79dd3494381c427db205
moryi <mory.ivan@gmail.com>
2022-08-17 00:19:34 +0300

    Remove some useless import

--- src/main/java/com/aston/studentslogs/Application.java
+++ src/main/java/com/aston/studentslogs/Application.java
@@ -1,15 +1,7 @@
 package com.aston.studentslogs;
 
 import com.aston.studentslogs.config.Config;
-import org.springframework.web.filter.CharacterEncodingFilter;
-import org.springframework.web.filter.HiddenHttpMethodFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
-
-import javax.servlet.DispatcherType;
-import javax.servlet.FilterRegistration;
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
-import java.util.EnumSet;
 
 public class Application extends AbstractAnnotationConfigDispatcherServletInitializer {
         @Override
